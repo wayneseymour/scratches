4ee739d4f33fb5b92c073682be3134572f0896e9
Tre' Seymour <wayne.seymour@elastic.co>
2022-08-09 14:20:42 +0100

    local.

--- take-generator-with-async-list.js
+++ take-generator-with-async-list.js
@@ -1,33 +1,34 @@
 const take3 = take(3);
+const take7 = take(7);
 const generate10 = numberGenerator(10);
 
 (async () => {
-  for await (const x of take3(generate10)) {
-    console.log(`\n### x: \n\t${x}`);
+  // for await (const x of take3(generate10)) {
+  for await (const x of take7(generate10)) {
   }
 })();
 
 function take(n) {
   var i = 0;
-  return async function* takeCollection (xs) {
+  return async function* takeCollection(xs) {
     for await (const x of xs) {
       if (n === i++) {
         return;
       }
       yield x;
     }
-  }
+  };
 }
 async function* numberGenerator(limit) {
   let i = 0;
   while (true) {
     await timer(Math.random() * 1000);
     yield i++;
-    if (i > limit) return;
+    if (i > 9) return;
   }
-};
+}
 function timer(ms) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-};
+}
