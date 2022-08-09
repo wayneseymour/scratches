68e12cba13bd49fb1614f9e6cf63969a4b054b30
Tre' Seymour <wayne.seymour@elastic.co>
2022-08-09 17:28:03 +0100

    more generators

---
+++ generator-vs-closure-limitless.js
@@ -0,0 +1,21 @@
+// from: https://egghead.io/lessons/javascript-use-a-generator-like-a-closure
+
+function* addingGenerator(a) {
+  const b = yield a
+  yield a + b
+}
+function addingClosure(a) {
+  return (b) => {
+    return a + b
+  }
+}
+function* addingGeneratorLimitless(a) {
+  let b = yield a
+  while (true) {
+    b = yield a + b
+  }
+}
+const add2Iterator = addingGeneratorLimitless(2)// Set initial state to 2
+console.log(add2Iterator.next()) // Observe initial state
+console.log(add2Iterator.next(1)) // Add initial state + 1 = { value: 3, done: false }
+console.log(add2Iterator.next(2))
