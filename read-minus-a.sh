b17afaa3f42d320c96aa8f46e966c3831bfd3683
Tre' Seymour <wayne.seymour@elastic.co>
2022-08-24 08:06:37 +0100

    Finally figured out how to appease shellcheck, for assigning arrays from commands

---
+++ read-minus-a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+standard_list="url,index-pattern,query,graph-workspace,tag,visualization,canvas-element,canvas-workpad,dashboard,search,lens,map,cases,uptime-dynamic-settings,osquery-saved-query,osquery-pack,infrastructure-ui-source,metrics-explorer-view,inventory-view,infrastructure-monitoring-log-view,apm-indices"
+
+arrayify_csv() {
+  local xs=${1}
+  echo "$xs" | tr ',' '\n' | uniq | sort
+}
+
+declare standardListArr=()
+while IFS='' read -r line; do standardListArr+=("$line"); done < <(arrayify_csv "$standard_list")
+
+echo "### standardListArr[*]: ${standardListArr[*]}"
