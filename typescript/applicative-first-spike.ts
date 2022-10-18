950d02c4098ff5b9418bc74b07f8da6542770b5d
Tre' Seymour <wayne.seymour@elastic.co>
2022-10-18 14:26:14 +0100

    Use ap to to curry function arguments while keeping the correct order of operations.

---
+++ typescript/applicative-first-spike.ts
@@ -0,0 +1,13 @@
+import * as O from 'fp-ts/lib/Option'
+import { Option } from 'fp-ts/lib/Option'
+import { pipe } from 'fp-ts/lib/function'
+
+const a: Option<number> = O.of(1)
+const b: Option<string> = O.of('some b')
+function foo(a: number, b: string): boolean {
+  return true
+}
+const fooC = (a: number) => (b: string) => foo(a, b)
+// Option<boolean>
+pipe(O.of(fooC), O.ap(a), O.ap(b))
+
