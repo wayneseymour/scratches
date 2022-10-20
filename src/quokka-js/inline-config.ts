cf42c052e14ff024d58c207e11c2177745829802
Tre' Seymour <wayne.seymour@elastic.co>
2022-10-20 11:57:43 +0100

    Add example inline conf for ts files to use js imports.

---
+++ src/quokka-js/inline-config.ts
@@ -0,0 +1,12 @@
+({
+  "ts": {
+    "compilerOptions": {
+      "allowJs": true
+    }
+  },
+  "logLimit": 200,
+  "delay": 1500
+})
+
+// The above must be placed at the top of a file for Quokka to recognize this.
+// Also, if already started, you've to restart quokka.
