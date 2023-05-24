1e1ca162ed9e9cbe3e926bce5577bd8805f1ed43
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-24 15:42:06 +0100

    try catch once, need a "keep trying" or try once more

---
+++ src/ts/fp/either/try-catch.ts
@@ -0,0 +1,48 @@
+import * as E from "fp-ts/lib/Either";
+import {resolve} from 'path';
+import {readFileSync} from 'fs';
+import {pipe} from "fp-ts/lib/function";
+
+interface FtrConfigWithOptions {
+  [configPath: string]: {
+    queue: string;
+  };
+}
+
+interface FtrConfigsManifest {
+  defaultQueue: string;
+  disabled: string[];
+  enabled: Array<string | FtrConfigWithOptions>;
+}
+
+
+const load = (x: string) =>
+  readFileSync(x, 'utf8');
+
+
+const FTR_CONFIGS_MANIFEST_REL = ".buildkite/ftr_configs.yml";
+// const FTR_CONFIGS_MANIFEST_REL = ".buildkite/NOPE/ftr_configs.yml";
+
+// const root = "/Users/trezworkbox/dev/main";
+const root = "/Users/trezworkbox/NOPE/dev/main.worktrees/from-pmuellers-pr";
+
+pipe(
+  E.tryCatch(
+    () => load(resolve(root, FTR_CONFIGS_MANIFEST_REL)),
+    // (_) => (_ instanceof Error ? _ : new Error('unknown error'))
+    E.toError,
+  ),
+  E.map((x) => ({
+    contents: x,
+    excerpt: 'blahbitty blah blah'
+  })),
+  // E.mapLeft((e) => ({
+  //   type: 'SomeOtherError',
+  //   error: E.toError(e),
+  // })),
+  E.fold(
+    (_: any) => _,
+    (x) => x.excerpt
+  )
+);
+
