0a63a0355d195777ab8ae407a4db7bd38546cbb9
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-29 11:48:09 +0100

    add ingore elements docs (copy-pasta)

--- src/ts/fp/rxjs/ignore-elements.ts
+++ src/ts/fp/rxjs/ignore-elements.ts
@@ -1,4 +1,15 @@
 import { of, ignoreElements } from 'rxjs'
+/**
+ * Ignores all items emitted by the source Observable and only passes calls of `complete` or `error`.
+ *
+ * The `ignoreElements` operator suppresses all items emitted by the source Observable,
+ * but allows its termination notification (either `error` or `complete`) to pass through unchanged.
+ *
+ * If you do not care about the items being emitted by an Observable, but you do want to be notified
+ * when it completes or when it terminates with an error, you can apply the `ignoreElements` operator
+ * to the Observable, which will ensure that it will never call its observers’ `next` handlers.
+ *
+ */
 
 of('you', 'talking', 'to', 'me')
   .pipe(ignoreElements())
