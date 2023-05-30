2c406936c58318e0ae20587275cc1a535e73d5e3
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-30 08:21:19 +0100

    start with zero task count.

--- src/js/rxjs/egghead.io/thinking-reactively/TaskProgressService.mjs
+++ src/js/rxjs/egghead.io/thinking-reactively/TaskProgressService.mjs
@@ -1,5 +1,5 @@
 import { Observable, merge } from 'rxjs'
-import { map, scan } from 'rxjs/operators';
+import { map, scan, startWith } from 'rxjs/operators';
 
 const taskStarts = new Observable();
 const taskCompletions = new Observable();
@@ -12,11 +12,12 @@ const loadDown = taskCompletions.pipe(map(-1));
 
 const loadVariations = merge(loadUp, loadDown);
 
-// When subscribed, the current task countl will be emitted into the stream.
+// When subscribed, the current task count will be emitted into the stream.
 const currentLoadCount = loadVariations.pipe(
+  startWith(0),
   scan((totalCurrentLoads, changeInLoads) => {
     return totalCurrentLoads + changeInLoads;
-  }, 0)
+  })
 )
 
 export default {};
