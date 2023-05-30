9764b86d313249088deb526ca81bd6105971d438
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-30 08:44:35 +0100

    Never emit less than zero tasks. For now, if we get way more completions than starts, it will keep emitting zero.

--- src/js/rxjs/egghead.io/thinking-reactively/TaskProgressService.mjs
+++ src/js/rxjs/egghead.io/thinking-reactively/TaskProgressService.mjs
@@ -5,7 +5,9 @@ const taskStarts = new Observable();
 const taskCompletions = new Observable();
 const showSpinner = new Observable();
 
+// increment
 const loadUp = taskStarts.pipe(map(1));
+// decrement
 const loadDown = taskCompletions.pipe(map(-1));
 
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx //
@@ -16,7 +18,10 @@ const loadVariations = merge(loadUp, loadDown);
 const currentLoadCount = loadVariations.pipe(
   startWith(0),
   scan((totalCurrentLoads, changeInLoads) => {
-    return totalCurrentLoads + changeInLoads;
+    // Video author added this in case task completions
+    // became much higher in count than task starts
+    const newLoadCount = totalCurrentLoads + changeInLoads;
+    return newLoadCount < 0 ? 0 : newLoad
   })
 )
 
