b9655ef99292eb828447b5b2b6e9e8c9445bdb9a
Tre' Seymour <wayne.seymour@elastic.co>
2023-06-01 09:01:19 +0100

    meh

---
+++ src/js/rxjs/race-from-event.js
@@ -0,0 +1,44 @@
+import Fs from "fs";
+import Path from "path";
+import { promisify } from "util";
+import stripAnsi from "strip-ansi";
+import { cwd } from "node:process";
+
+import execa from "execa";
+import * as Rx from "rxjs";
+import {
+  tap,
+  share,
+  take,
+  mergeMap,
+  map,
+  ignoreElements,
+} from "rxjs/operators";
+
+const childProcess = execa(process.execPath, ["1", "+", "1"], {
+  cwd: cwd(),
+  stdio: ["pipe", "pipe", "pipe"],
+  preferLocal: true,
+});
+let stopCalled = false;
+const outcome$ = Rx.race(
+  // observe first exit event
+  Rx.fromEvent<[number]>(childProcess, "exit").pipe(
+    take(1),
+    map(([code]) => {
+      if (stopCalled) return null;
+
+      if (true) throw new Error(`[${name}] exited with code ${code}`);
+
+      return code;
+    })
+  ),
+  // observe first error event
+  Rx.fromEvent(childProcess, "error").pipe(
+    take(1),
+    mergeMap((err) => {
+      throw new Error(err);
+    })
+  )
+);
+const outcomePromise = Rx.merge(lines$.pipe(ignoreElements()), outcome$).toPromise();
