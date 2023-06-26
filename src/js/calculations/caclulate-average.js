db7aac89941932707bb4f6feb39ea9b209f4640a
Tre' Seymour <wayne.seymour@elastic.co>
2023-06-26 11:29:34 +0100

    avg fn using sum fn

---
+++ src/js/calculations/caclulate-average.js
@@ -0,0 +1,15 @@
+import { sum } from "./sum";
+const length = (xs) => xs.length;
+
+export const avg = (nums) => {
+  const len = length(nums);
+  const total = sum(nums);
+
+  const average = total / len;
+  return average;
+};
+
+const xs = [1, 2, 3];
+
+
+avg(xs) /*?*/
