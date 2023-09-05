498730448d5c113516d9b34ba1c2cf12d49107b9
Tre' Seymour <wayne.seymour@elastic.co>
2023-09-05 12:52:08 +0100

    it works

---
+++ src/projects/is-es-arch-slow/combine-decompression-and-json-stanza-streams/readable-to-gunzip-to-jsonstanza-stream.mjs
@@ -0,0 +1,43 @@
+import { interval, take, range, concat } from "rxjs";
+import { from, fromEventPattern } from "rxjs";
+import { map } from "rxjs/operators";
+import * as fs from "fs";
+import oboe from "oboe";
+import * as zlib from "zlib";
+
+
+const archivePath = "myfarequote.txt";
+const compressedArchiveFilePath =
+  // "/Users/trezworkbox/dev/main.worktrees/can-we-oboe/x-pack/test/functional/es_archives/ml/farequote/data.json.gz";
+  "/Users/trezworkbox/dev/main.worktrees/straight-pipe-using-sax-parser/x-pack/test/functional/es_archives/logstash_functional/data.json.gz";
+
+const toStr = (x) => `${x}`;
+const noop = () => {};
+
+const begin = (tgt) => {
+  const obj$ = (x) =>
+    oboe(fs.createReadStream(x).pipe(zlib.createGunzip()))
+
+
+  const json$ = () => obj$(tgt).on("done", (...args) => {
+    console.log(`\nλjs args: \n${JSON.stringify(args, null, 2)}`)
+  });
+
+  json$(tgt)
+
+};
+
+begin(compressedArchiveFilePath)
+
+const jsonStanzaStream = (pathToFile) => (_) =>
+  oboe(fs.createReadStream(pathToFile)).on("done", _);
+const jsonStanzaObservable = (jsonStanza$) => (x) => () =>
+  fromEventPattern(jsonStanza$(x));
+
+// const result = concat(
+// decompressionObservable(archivePath)(),
+// jsonStanzaObservable(jsonStanzaStream)(archivePath)()
+// );
+// jsonStanzaObservable(jsonStanzaStream)(archivePath)().subscribe((x) =>
+//   console.log(x)
+// );
--- src/projects/is-es-arch-slow/combine-decompression-and-json-stanza-streams/two-separate-file-streams-wont-work.mjs
+++ src/projects/is-es-arch-slow/combine-decompression-and-json-stanza-streams/two-separate-file-streams-wont-work.mjs
@@ -31,21 +31,46 @@ import * as zlib from "zlib";
 // subscribeToDecompressionStream(decompressionObservable(archivePath));
 // subscribeToStreamingJsonStream(archivePath);
 
-const archivePath =
-  "/Users/trezworkbox/dev/scratches/src/js/streams/native-nodejs-streams/gunzip/someotherfile.txt.gz";
-// "/Users/trezworkbox/dev/main.worktrees/can-we-oboe/x-pack/test/functional/es_archives/ml/farequote/data.json.gz";
+const archivePath = "myfarequote.txt";
+const compressedArchivePath =
+  "/Users/trezworkbox/dev/main.worktrees/can-we-oboe/x-pack/test/functional/es_archives/ml/farequote/data.json.gz";
 
 const toStr = (x) => `${x}`;
-const decompressionObservable = (x) => () =>
-  from(fs.createReadStream(x).pipe(zlib.createGunzip())).pipe(map(toStr));
+const noop = () => {};
+
+const begin = (tgt) => {
+  const obj$ = (x) =>
+    oboe(fs.createReadStream(x).pipe(zlib.createGunzip())).pipe(map(toStr));
+  const json$ = () => obj$(tgt).on("done", noop);
+
+  fromEventPattern(json$).subscribe({
+    next: (x) => console.log(`\nλjs x: \n${JSON.stringify(x, null, 2)}`),
+    error: (err) => console.log("error:", err),
+    complete: () => console.log("the end"),
+  });
+};
+
+// begin(compressedArchivePath);
+
+// const pipedOboeStream = (x) => (_) =>
+//   oboe(
+//     fs
+//       .createReadStream(x)
+//       .pipe(zlib.createGunzip())
+//       .pipe(map(toStr))
+//   ).on("done", _)
+// fromEventPattern(pipedOboeStream(archivePath)())
+//   .subscribe((x) => console.log(x));
 
 const jsonStanzaStream = (pathToFile) => (_) =>
   oboe(fs.createReadStream(pathToFile)).on("done", _);
 const jsonStanzaObservable = (jsonStanza$) => (x) => () =>
   fromEventPattern(jsonStanza$(x));
 
-const result = concat(
-  decompressionObservable(archivePath)(),
-  jsonStanzaObservable(jsonStanzaStream)(archivePath)()
+// const result = concat(
+// decompressionObservable(archivePath)(),
+// jsonStanzaObservable(jsonStanzaStream)(archivePath)()
+// );
+jsonStanzaObservable(jsonStanzaStream)(archivePath)().subscribe((x) =>
+  console.log(x)
 );
-result.subscribe((x) => console.log(x));
